1a239109b772b4a3ef45c46787c4ea0cddb32273
Victor Turturika <v.turturika@gmail.com>
2015-09-24 23:02:11 +0300

    Added support different sizes of matrices

--- matrixMult/matrixMult/Source.cpp
+++ matrixMult/matrixMult/Source.cpp
@@ -9,35 +9,47 @@ using namespace std;
 
 int main() {
 
-	int N;
+	int aRows, aColumns, bRows, bColumns;
 	int **a, **b, **c;
 	int sum = 0;
 	string answer = "";
 
-	cout << "Input N (max 13000)\nN = ";
-	cin >> N;
+	cout << "Input sizes of matrix A and B (max 13'000)\n\n";
+	cout << "First matrix : Rows Columns = ";
+	cin >> aRows >> aColumns;
+	cout << "Second matrix : Rows Columns = ";
+	cin >> bRows >> bColumns;
+
+	if (aColumns != bRows) {
+
+		cout << "\nError!\nMatrices can't be multiplied\n";
+		cin.get(); cin.get();
+		return 1;
+	}
 
 	clock_t t;
 	t = clock();
 
 	try {
 
-		a = new int*[N];
-		b = new int*[N];
-		c = new int*[N];
+		a = new int*[aRows];
+		b = new int*[bRows];
+		c = new int*[aRows];
 
-		for (int i = 0; i < N; i++) {
+		int maxRowCount = (aRows > bRows) ? aRows : bRows;
 
-			a[i] = new int[N];
-			b[i] = new int[N];
-			c[i] = new int[N];
+		for (int i = 0; i < maxRowCount; i++) {
+
+			if(i < aRows) a[i] = new int[aColumns];
+			if(i < bRows) b[i] = new int[bColumns];
+			if(i < aRows) c[i] = new int[bColumns];
 		}
 	}
 	catch (bad_alloc) {
 
 		cout << "Bad alloc\n";
-		cin.get();
-		return 1;
+		cin.get(); cin.get();
+		return 2;
 
 	}
 
@@ -49,39 +61,45 @@ int main() {
 	srand(time(NULL));
 	t = clock();
 
+	int maxRowCount = (aRows > bRows) ? aRows : bRows;
+	int maxColumnCount = (aColumns > bColumns) ? aColumns : bColumns;
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++) {
+	for (int i = 0; i < maxRowCount; i++) {
 
-			a[i][j] = rand() % 200 - 100;
-			b[i][j] = rand() % 200 - 100;
+		for (int j = 0; j < maxColumnCount; j++) {
+
+			if (i < aRows && j < aColumns) a[i][j] = rand() % 200 - 100;
+			if (i < bRows && j < bColumns)  b[i][j] = rand() % 200 - 100;
 		}
+	}
 
 	auto filling = double((clock() - t)) / CLOCKS_PER_SEC;
 
 	cout << "Filling time : " << setprecision(30) << filling
-		<< endl;
+		 << endl;
 
 	cout << "Work in progress...\n";
 
 	srand(time(NULL));
 	t = clock();
 
-	for (int i = 0; i < N; i++) {
-		for (int d = 0; d < N; d++) {
-			for (int k = 0; k < N; k++) {
-				sum = sum + a[i][k] * b[k][d];
+	for (int i = 0; i < aRows; i++) {
+
+		for (int j = 0; j < bColumns; j++) {
+
+			for (int k = 0; k < aColumns; k++) {
+
+				sum = sum + a[i][k] * b[k][j];
 			}
 
-			c[i][d] = sum;
+			c[i][j] = sum;
 			sum = 0;
 		}
 	}
 
 	auto execution = double((clock() - t)) / CLOCKS_PER_SEC;
 
-	cout << "Execution time : " << setprecision(30) << execution
-		<< endl;
+	cout << "Execution time : " << setprecision(30) << execution << endl;
 
 	cout << "Do you wan't to save results of multiplcation in file? Y/N: ";
 	cin >> answer;
@@ -90,14 +108,14 @@ int main() {
 		ofstream fout;
 		fout.open("results.txt");
 
-		fout << "Allocation time: " << setprecision(30) << allocation << endl
-			<< "Filling time:\t " << filling << endl
-			<< "Execution time:\t " << execution << endl;
+		fout << "Allocation time: "  << setprecision(30) << allocation << endl
+			 << "Filling time:\t "   << filling   << endl
+			 << "Execution time:\t " << execution << endl;
 		fout << "Results:\n";
 
-		for (int i = 0; i < N; i++) {
-			for (int d = 0; d < N; d++)
-				fout << c[i][d] << "\t";
+		for (int i = 0; i < aRows; i++) {
+			for (int j = 0; j < bColumns; d++)
+				fout << c[i][j] << "\t";
 			fout << endl;
 		}
 	}
@@ -108,11 +126,11 @@ int main() {
 	cout << "Press <Enter> to clean memory\n";
 	cin.get(); cin.get();
 
-	for (int i = 0; i<N; i++) {
+	for (int i = 0; i<maxRowCount; i++) {
 
-		delete[] a[i];
-		delete[] b[i];
-		delete[] c[i];
+		if( i < aRows ) delete[] a[i];
+		if( i < bRows ) delete[] b[i];
+		if( i < aRows ) delete[] c[i];
 	}
 
 	delete[] a;
